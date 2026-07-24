64ab8e80677b30e4ee5d31df5347bf033c247f13
abedsaeed <100998140+abedsaeed@users.noreply.ub.com>
2026-07-24 17:35:14 +0300

    text field and how to use controllers for ti

--- lib/Views/pages/Profilepage.dart
+++ lib/Views/pages/Profilepage.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class Progile_page extends StatelessWidget {
+class Progile_page extends StatefulWidget {
   const Progile_page({super.key});
 
   @override
+  State<Progile_page> createState() => _Progile_pageState();
+}
+
+class _Progile_pageState extends State<Progile_page> {
+  TextEditingController ControllerfromProfile = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Page', style: TextStyle(fontSize: 24)));
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          TextField(
+            controller: ControllerfromProfile,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Enter your name',
+            ),
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+          Text('Your name is: ${ControllerfromProfile.text}'),
+        ],
+      ),
+    );
   }
 }
